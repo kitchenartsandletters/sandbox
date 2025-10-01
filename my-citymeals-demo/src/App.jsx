b688d471c1fb9dfe2df8c4a824e54b5a02db0c02
ghoulcauldron <gilcalderon07@gmail.com>
2025-10-01 17:02:43 -0400

    adding sandbox index

--- my-citymeals-demo/src/App.jsx
+++ my-citymeals-demo/src/App.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import './App.css'
 import './index.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import CitymealsLandingMockup from './CitymealsLandingMockup';
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Sandbox Demos</h1>
+      <div className="card">
+        <h2>Citymeals Landing Page Mockup</h2>
+        <p>A simple landing page mockup for Citymeals.</p>
+        <Link to="/citymeals" className="btn">View Demo</Link>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<CitymealsLandingMockup />} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/citymeals" element={<CitymealsLandingMockup />} />
         {/* Future sandboxes here */}
       </Routes>
     </BrowserRouter>
